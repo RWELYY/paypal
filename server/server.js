5266ff9e32134d154ae1da5dc0a83b25b37298ac
RWELYY <103866558+RWELYY@users.noreply.github.com>
2024-11-29 21:37:59 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -131,8 +131,8 @@ app.listen(PORT, () => {
 });
 
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/src/dist")));
 
   app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../client", "src", "dist", "index.html"))
   })
